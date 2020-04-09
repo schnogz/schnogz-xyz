346a5f2069eb07e57b258a2499fdd93d11897af2
schnogz <andrew@blockchain.com>
2020-04-09 17:46:39 -0400

    fix(performance): preconnect to commonly requested domains :zap:

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,19 +2,19 @@ const path = require('path')
 
 module.exports = {
   siteMetadata: {
-	  siteUrl: 'http://www.schnogz.com',
-    title: 'Andrew Schneider',
+    siteUrl: 'http://www.schnogz.com',
+    title: 'Andrew Schneider (Schnogz)',
   },
   plugins: [
     {
       resolve: 'gatsby-plugin-root-import',
       options: {
-	      components: path.join(__dirname, 'src/components'),
-	      content: path.join(__dirname, 'src/content'),
-	      fonts: path.join(__dirname, 'src/fonts'),
-	      img: path.join(__dirname, 'src/img'),
-	      layouts: path.join(__dirname, 'src/layouts'),
-	      pages: path.join(__dirname, 'src/pages'),
+        components: path.join(__dirname, 'src/components'),
+        content: path.join(__dirname, 'src/content'),
+        fonts: path.join(__dirname, 'src/fonts'),
+        img: path.join(__dirname, 'src/img'),
+        layouts: path.join(__dirname, 'src/layouts'),
+        pages: path.join(__dirname, 'src/pages'),
         src: path.join(__dirname, 'src'),
         styles: path.join(__dirname, 'src/styles'),
         utils: path.join(__dirname, 'src/utils'),
@@ -22,12 +22,21 @@ module.exports = {
     },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-	  'gatsby-plugin-sitemap',
-	  'gatsby-plugin-robots-txt',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-robots-txt',
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-62555232-1',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-preconnect',
+      options: {
+        domains: [
+          'https://www.google-analytics.com',
+          'https://www.schnogz.com',
+        ],
       },
     },
   ],
