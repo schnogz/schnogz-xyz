cc987a2182e476ea8c8dbb98046c785c2fb1c3e0
schnogz <andrew@blockchain.com>
2020-04-18 17:45:59 -0400

    fix(lint issues): remove unused exports and add a11y to emojis :rotating_light:

--- src/content/projects.js
+++ src/content/projects.js
@@ -18,12 +18,18 @@ export default () => (
           <Project
             abstract="My personal blog where I write about the things that keep me going. Musings will include coding, music, tech, photography, travel and much more."
             logo={
-              <EmojiIcon role="img" aria-label="writing emoji">
-                ✍🏼️
+              <EmojiIcon>
+                <span role="img" aria-label="writing emoji">
+                  ✍🏼️
+                </span>
               </EmojiIcon>
             }
             subtitle={
-              <a href="https://theonist.com" target="_blank">
+              <a
+                href="https://theonist.com"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
                 https://theonist.com
               </a>
             }
@@ -33,11 +39,17 @@ export default () => (
             abstract="A website that decodes Ethereum blockchain transaction (including ERC20) hashes into readable JSON."
             logo={
               <EmojiIcon role="img" aria-label="open lock emoji">
-                🔓
+                <span role="img" aria-label="open lock emoji">
+                  🔓
+                </span>
               </EmojiIcon>
             }
             subtitle={
-              <a href="https://ethereumdecoder.com" target="_blank">
+              <a
+                href="https://ethereumdecoder.com"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
                 https://ethereumdecoder.com
               </a>
             }
@@ -46,13 +58,14 @@ export default () => (
           <Project
             abstract="An iTerm2 plugin that displays various stats about the Bitcoin blockchain as well as price information of many other top cryptocurrencies."
             logo={
-              <EmojiIcon role="img" aria-label="chainlinks emoji">
-                ⛓
+              <EmojiIcon>
+                <span aria-label="chainlinks emoji">⛓</span>
               </EmojiIcon>
             }
             subtitle={
               <a
                 href="https://github.com/schnogz/iterm-blockchain-components"
+                rel="noopener noreferrer"
                 target="_blank"
               >
                 https://github.com/schnogz/iterm-blockchain-components
@@ -63,13 +76,16 @@ export default () => (
           <Project
             abstract="A NodeJS wrapper for Songkick's full REST API available on both NPM and GitHub."
             logo={
-              <EmojiIcon role="img" aria-label="package emoji">
-                📦
+              <EmojiIcon>
+                <span role="img" aria-label="package emoji">
+                  📦
+                </span>
               </EmojiIcon>
             }
             subtitle={
               <a
                 href="https://github.com/schnogz/songkick-api-node"
+                rel="noopener noreferrer"
                 target="_blank"
               >
                 https://github.com/schnogz/songkick-api-node
